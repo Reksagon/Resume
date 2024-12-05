85987ffd1026b7d2610b90cc788a9ad515b4e796
Denys Korniienko <71882300+Reksagon@users.noreply.github.com>
2024-12-05 12:11:53 +0200

    Create custom_en.js

---
+++ js/custom_en.js
@@ -0,0 +1,202 @@
+// JavaScript Document
+
+$(window).load(function () {
+    "use strict";
+    // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+
+$(document).ready(function () {
+    "use strict";
+
+    // scroll menu
+    var sections = $('.section'),
+        nav = $('.navbar-fixed-top,footer'),
+        nav_height = nav.outerHeight();
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        var $el = $(this),
+            id = $el.attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - nav_height + 2
+        }, 600);
+
+        return false;
+    });
+
+
+    // Menu opacity
+    if ($(window).scrollTop() > 80) {
+        $(".navbar-fixed-top").addClass("bg-nav");
+    } else {
+        $(".navbar-fixed-top").removeClass("bg-nav");
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 80) {
+            $(".navbar-fixed-top").addClass("bg-nav");
+        } else {
+            $(".navbar-fixed-top").removeClass("bg-nav");
+        }
+    });
+
+
+
+    // Parallax
+    var parallax = function () {
+        $(window).stellar();
+    };
+
+    $(function () {
+        parallax();
+    });
+
+    // AOS
+    AOS.init({
+        duration: 1200,
+        once: true,
+        disable: 'mobile'
+    });
+
+    //  isotope
+    $('#projects').waitForImages(function () {
+        var $container = $('.portfolio_container');
+        $container.isotope({
+            filter: '*',
+        });``
+
+        $('.portfolio_filter a').click(function () {
+            $('.portfolio_filter .active').removeClass('active');
+            $(this).addClass('active');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 500,
+                    animationEngine: "jquery"
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+    function openPage(url) {
+        window.location.href = url; // Перенаправляємо на нову сторінку
+    }
+    
+    // Додаємо події на кнопки для відкриття нових сторінок
+    document.getElementById('demo01').addEventListener('click', function() {
+        openPage('project1_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo02').addEventListener('click', function() {
+        openPage('project2_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo03').addEventListener('click', function() {
+        openPage('project3_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo04').addEventListener('click', function() {
+        openPage('project4_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo05').addEventListener('click', function() {
+        openPage('project5_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo06').addEventListener('click', function() {
+        openPage('project6_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo07').addEventListener('click', function() {
+        openPage('project7_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo08').addEventListener('click', function() {
+        openPage('project8_en.html'); // Відкриваємо сторінку 1
+    });
+
+    document.getElementById('demo09').addEventListener('click', function() {
+        openPage('project9_en.html'); // Відкриваємо сторінку 1
+    });
+
+    // validate contact form
+    $(function () {
+        $('#contact-form').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true
+                },
+                phone: {
+                    required: false
+                },
+                message: {
+                    required: true
+                }
+
+            },
+            messages: {
+                name: {
+                    required: "This field is required",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                email: {
+                    required: "This field is required"
+                },
+                message: {
+                    required: "This field is required"
+                }
+            },
+            submitHandler: function (form) {
+                $(form).ajaxSubmit({
+                    type: "POST",
+                    data: $(form).serialize(),
+                    url: "process.php",
+                    success: function () {
+                        $('#contact :input').attr('disabled', 'disabled');
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor', 'default');
+                            $('#success').fadeIn();
+                        });
+                    },
+                    error: function () {
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $('#error').fadeIn();
+                        });
+                    }
+                });
+            }
+        });
+
+    });
+});
